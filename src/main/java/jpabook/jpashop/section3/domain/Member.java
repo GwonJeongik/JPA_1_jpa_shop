6dc258b002d7b9beeb9540bbe5a23cd5245317b8
ik <jeongik11029@naver.com>
2024-10-04 23:19:37 +0900

    [update] : Member.name에 유니크 제약조건 추가

--- src/main/java/jpabook/jpashop/section3/domain/Member.java
+++ src/main/java/jpabook/jpashop/section3/domain/Member.java
@@ -2,12 +2,14 @@ package jpabook.jpashop.section3.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member {
 
     @Id
@@ -15,14 +17,15 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @Column(unique = true)
+    private String name;
+
     @Embedded
     private Address address;
 
     //Member를 저장, 삭제 할 때, Order을 전파시킬 이유가 없음. <- `Member`와 `Order`은 독립적인 생존주기를 가지는 게 자연스럽다.
     @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
-
-    private String name;
+    private final List<Order> orders = new ArrayList<>();
 
     public Member(String name) {
         this.name = name;
