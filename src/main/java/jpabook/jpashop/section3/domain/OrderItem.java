1cd36da444865c7289840bc6f78f3510dd4fae5f
ik <jeongik11029@naver.com>
2024-10-06 22:41:52 +0900

    [update] : OrderItem에 생성, 비지니스, 조회 로직 추가

--- src/main/java/jpabook/jpashop/section3/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/section3/domain/OrderItem.java
@@ -25,7 +25,33 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    public void changeOrder(Order order) {
+    public void putOrder(Order order) {
         this.order = order;
+    }
+
+    //==생성 로직==//
+    /* 상품 주문 생성 메서드 */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.item = item;
+        orderItem.orderPrice = orderPrice;
+        orderItem.count = count;
+
+        //1. 상품 주문 생성할 때, 상품의 전체 재고 감소//
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    /* 취소된 상품 수량 원복 메서드 */
+    public void cancel() {
+        item.addStock(count);
+    }
+
+    //==조회 로직==//
+    /* 주문 상품의 가격 조회 메서드 */
+    public int getTotalPrice() {
+        return orderPrice * count;
     }
 }
